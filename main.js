17db9386a37a245829f65bb7c1245485d64dacbc
Kyungho0511 <kleearchitecture@gmail.com>
2023-05-23 00:05:50 +0900

    JS: Scroll to section

--- main.js
+++ main.js
@@ -10,3 +10,24 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// navbar menu item buttons scroll to the section
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', event => {
+    const menu = event.target.dataset.menu;
+    document.querySelector('.navbar__menu__item.active').classList.remove('active');
+    event.target.classList.add('active');
+    if (!menu) return;
+    scrollIntoView(menu);
+});
+
+// contact me button scroll to the section
+const contactMe = document.querySelector('.home__contact');
+contactMe.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
